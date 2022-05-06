20544ba15e865867a786f4a851b064115cf7e41d
andradeviniicius <viniciusdandrade01@gmail.com>
2022-05-06 13:27:53 -0300

    primeira tennnntativa

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ submitNewTask.addEventListener('click',(e)=>{
     createTask(taskName.value)
     taskName.value = ''
 })
+
+
+
+
+const addTaskButtonMobile = document.querySelector('.addButton')
+
+
+addTaskButtonMobile.addEventListener('click',()=>{
+    const body = document.querySelector('body')
+    const a = document.createElement('div')
+    
+    
+    if(document.querySelector('.modal')){
+        body.removeChild(document.querySelector('.modal'))
+        addTaskButtonMobile.classList.remove('active')
+    } else {
+        addTaskButtonMobile.classList.toggle('active')
+    
+        a.classList.add('modal')
+        a.classList.toggle('active')
+    
+        taskName.parentElement.classList.add('active')
+        a.appendChild(taskName.parentElement)
+        body.insertBefore(a,body.firstChild)
+        a.addEventListener('onkeypress',()=>{
+            if(document.querySelector('.modal')){
+                body.removeChild(document.querySelector('.modal'))
+                addTaskButtonMobile.classList.remove('active')
+            }
+        })
+    }
+    
+})
